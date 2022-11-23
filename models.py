b18309f05dfbcf1a2c111ded1c21e39c5c992f5d
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-23 13:44:09 -0300

    Correción de username

--- models.py
+++ models.py
@@ -6,17 +6,17 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
 
     def serialize (self):
         return {
             "id": self.id,
-            "username": self.username,
+            "username": self.name,
             "email": self.email,
             "password": self.password,
         }
 
     def __repr__(self) -> str:
-        return "<User %r>" % self.username
+        return "<User %r>" % self.name
