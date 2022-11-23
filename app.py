0aeae6eab4c2f60176290f67b4e7d481d5a9b904
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-23 13:01:24 -0300

    db init, db migrate y db upgrade

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 from models import db
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-CORS(app)
-db.init_app(app)
+
+# Configuración
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
     os.path.join(BASEDIR, "auth.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["ENV"] = "development"
+
+CORS(app)
+db.init_app(app)
+Migrate(app, db)
