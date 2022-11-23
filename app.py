4e66551e2164bf46503040c2724c7fa02a91fe42
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-23 13:55:34 -0300

    Creación de usuario

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from models import db, User
+
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,10 +16,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
     os.path.join(BASEDIR, "auth.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["ENV"] = "development"
+app.config["SECRET_KEY"] = "super_secret_key"
 
 CORS(app)
 db.init_app(app)
 Migrate(app, db)
+bcrypt = Bcrypt(app)
 
 
 # Rutas
@@ -34,10 +38,24 @@ def user():
     password = request.json.get("password")
 
     found_user = User.query.filter_by(email=email).first()
+    print(found_user)
     if found_user is not None:
         return jsonify({
             "msg": "Email is already in use"
         }), 400
+
+    user.email = email
+    user.name = name
+    password_hash = bcrypt.generate_password_hash(password)
+    user.password = password_hash
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "msg": "success creating user"
+        "data": user.serialize()
+    }), 200
 
 
 # Configuración Servidor
